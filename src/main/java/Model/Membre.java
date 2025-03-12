1719fe1f9993fd7910d468eb3b683009cfaea19c
Oumayma Saidi <your_email@example.com>
2025-03-12 11:30:41 +0000

    first commit

---
+++ src/main/java/Model/Membre.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.util.Date;
+
+    public class Membre {
+        private String nom;
+        private Date datenaissance;
+        private String sport_pratique;
+    }
